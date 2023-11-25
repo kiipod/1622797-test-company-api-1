c3973e98c0eec54248785702c1ff97df5aa1808e
kiipod <iamkir90@hotmail.com>
2023-11-25 14:46:37 +0300

    Добавляет контроллеры для сущностей

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    /**
+     * Метод отвечает за показ всех комментариев у компании
+     *
+     * @return void
+     */
+    public function index(int $companyId)
+    {
+        //
+    }
+
+    /**
+     * Метод отвечает за создание комментария
+     *
+     * @return void
+     */
+    public function create(int $companyId)
+    {
+        //
+    }
+
+    /**
+     * Метод отвечает за редактирование комментария
+     *
+     * @return void
+     */
+    public function edit(int $commentId)
+    {
+        //
+    }
+
+    /**
+     * Метод отвечает за удаление комментария
+     *
+     * @return void
+     */
+    public function destroy(int $commentId)
+    {
+        //
+    }
+}
